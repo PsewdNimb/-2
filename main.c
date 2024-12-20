4cd251f281b22b49d35f76d07544d257ea5d6218
PsewdaNimb <psewdanimb@gmail.com>
2024-12-21 02:17:51 +0300

    All files create

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
-#include "regex.h"
-int main(){
+#include"regex.h"
+int main(size_t argc, char** argv){
 
     return 0;
 }
---
+++ regex.h
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool errorChecker(size_t argc, char **argv);
+
+void command(size_t argc, char **argv);
